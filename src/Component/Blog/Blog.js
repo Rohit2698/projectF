f62ed59bf1f04d837496a28c30a2412548d72e83
Rohit2698 <36307148+Rohit2698@users.noreply.github.com>
2020-06-18 22:28:55 +0530

    Making Code Cleaner

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -6,7 +6,15 @@ import img3 from '../../image/3.JPG';
 import img4 from '../../image/4.JPG';
 import Motivational from '../Motivational/Motivational';
 class Blog extends Component{
+    constructor(props){
+        super(props);
+        this.state=(
+            {img:[img1,img2,img3,img4]}
+        )
+    };
+
     render(){
+        const img=this.state.img;
         return(
             <div className="containerBlog">
                 <span className="title">Anti-fit and sustainable fashion wear infused with stories, love and creativity</span>
@@ -14,10 +22,7 @@ class Blog extends Component{
                 <Motivational/>
                 <div className="BlogDescription">
                 <div className="image">
-                    <img src={img1} alt="img1"/>
-                    <img src={img2} alt="img1"/>
-                    <img src={img3} alt="img1"/>
-                    <img src={img4} alt="img1"/>
+                {img.map((img)=>(<img src={img} alt="img1"/>))}
                 </div>
                 <p>Inspiration for our apparels - A collection for desi Dora, jodhpur. We wanted to design a collection that gave comfort to the wearer demand at any moment. The idea of anti-fit clothing is at its peak as it represents the society that talks about freedom and breaking barrier and also makes the wearer stronger. The silhouettes are mostly relaxed and oversized, designed to keep in mind the overall aesthetic of the brand ( a door to the modern and cultural style of India). We kept the fabrics very light woven and tunics designed with pockets. The leftover fabric scraps were converted into small potlis to make sure that no fabric is wasted.</p>
                 <center><iframe title="video" width="560" height="315" src="https://www.youtube.com/embed/XqZsoesa55w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
