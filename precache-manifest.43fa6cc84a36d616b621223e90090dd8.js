9b058a60cdf8a88d7cfc02fd82f1db424ba6f9ec
Rohit2698 <36307148+Rohit2698@users.noreply.github.com>
2020-06-18 17:05:38 +0530

    Updates

---
+++ precache-manifest.43fa6cc84a36d616b621223e90090dd8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f466b4893d6e9116669ea8b91d9f2dc8",
+    "url": "/projectF/index.html"
+  },
+  {
+    "revision": "f470c3d2f463cf7be0ca",
+    "url": "/projectF/static/css/main.88634451.chunk.css"
+  },
+  {
+    "revision": "75e01fc4bbc8507d3e67",
+    "url": "/projectF/static/js/2.9aa428d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectF/static/js/2.9aa428d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f470c3d2f463cf7be0ca",
+    "url": "/projectF/static/js/main.80231fad.chunk.js"
+  },
+  {
+    "revision": "7f87ef77ba636fc855db",
+    "url": "/projectF/static/js/runtime-main.6d445719.js"
+  },
+  {
+    "revision": "0c59f1e79bedcb85971ed14947b6b867",
+    "url": "/projectF/static/media/1.0c59f1e7.JPG"
+  },
+  {
+    "revision": "3359beab654ec002a48f78eea68af316",
+    "url": "/projectF/static/media/2.3359beab.JPG"
+  },
+  {
+    "revision": "dd22274748891de7c4c7b01614812368",
+    "url": "/projectF/static/media/3.dd222747.JPG"
+  },
+  {
+    "revision": "d037f7379818498987bc8f65f457eff3",
+    "url": "/projectF/static/media/4.d037f737.JPG"
+  }
+]);
